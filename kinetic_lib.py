4ebb1ae902f56a2db42f521b7c44bf0fd23b473f
skrepka <>
2024-01-12 01:02:02 +0300

    upd, many fixes, but still not working

--- kinetic_lib.py
+++ kinetic_lib.py
@@ -1,4 +1,4 @@
-"""Kinetic library version 0.02
+"""Kinetic library version 0.03
 with the capability to calculate various thermodynamic properties for arbitrary temperature:
 
     Statistical Sums: Total/Translational/Internal
@@ -35,13 +35,15 @@ class Particle:
     def __init__(self, name):
         """
         Particle initialization
+        Spectroscopic data is coming from the particle_data dictionary.
 
         Parameters:
         - name (string): the name of the particle, e.g. N2, N
         """
         self.name = name
         self.particle_data = self.particle_data[name]
-        self.mass = self.particle_data["m"] / G_TO_KG
+        self.mole_number = 1
+        self.mass = self.particle_data["M"] / G_TO_KG / self.constants["N_a"]
         self.R_specific = self.constants["k_B"] * self.constants["N_a"] / self.mass
 
     def __repr__(self):  # to print in console environment
@@ -64,10 +66,17 @@ class Particle:
 
     def _eps_el(self, n):
         """
-        Electronic energy for n-th electronic state
+        Electronic energy for n-th electronic state.
+        Tabulated in particle_data.
+        $$\varepsilon_{el} = E_{el}hc$$
+
+        Parameters:
+        - n: electronic level
+
         """
         hc = self.constants["h"] * self.constants["c"]
-        return self.particle_data["eps_el_n"][n] * hc * 100
+        eps_el = self.particle_data["eps_el_n"][n] * hc * 100
+        return eps_el
 
     def _eps_v(self, n, i):
         """
@@ -79,11 +88,12 @@ class Particle:
         + \omega^{c,n}_{e}y^{c,n}_{e}\Big(i+\frac{1}{2}\Big)^3\Big)$$
         """
         hc = self.constants["h"] * self.constants["c"]
-        # alt model with I_c
+        # TODO: alt model with harmonic osc and I_c
         eps_divbyhc = (
             self.particle_data["omega_n"][n] * (i + 0.5)
             - self.particle_data["omega_ex_n"][n] * (i + 0.5) ** 2
             + self.particle_data["omega_ey_n"][n] * (i + 0.5) ** 3
+            + self.particle_data["omega_ez_n"][n] * (i + 0.5) ** 4
         )
         return eps_divbyhc * hc * CM_TO_M
 
@@ -95,6 +105,7 @@ class Particle:
         $$B^{c}_{ni} = B^{c}_{n,e} - \alpha^{c}_{n,e}\Big(i + \frac{1}{2}\Big)$$
         $$D^{c}_{ni} = D^{c}_{n,e} - \beta^{c}_{n,e}\Big(i + \frac{1}{2}\Big)$$
         """
+        # TODO: parameters for rigid rotator
         hc = self.constants["h"] * self.constants["c"]
         B_ni = self.particle_data["B_n"][n] - self.particle_data["alpha_n"][n] * (
             i + 0.5
@@ -133,7 +144,6 @@ class Particle:
                 2
                 * np.pi
                 * self.mass
-                / self.constants["N_a"]
                 * self.constants["k_B"]
                 * T
                 / self.constants["h"] ** 2
@@ -149,53 +159,58 @@ class Particle:
 
     def c_v(self, T):
         """
-        Unit heat capacity at constant volume
+        Heat capacity at constant volume
         $$c_{v} = c_{v,tr} + c_{v,rot}$$
         """
         return self.c_v_int(T) + self.c_v_tr()
 
     def c_v_tr(self, T=None):
         """
-        Translational unit heat capacity at constant volume
+        Translational heat capacity at constant volume
         $$c_{v,tr} = \frac{3}{2}\frac{k}{m}$$
         """
-        return (3 / 2) * self.R_specific
+        return (3 / 2) * self.constants["k_B"] / self.mass
 
     @abstractmethod
     def c_v_int(self, T):
         """
-        Internal unit heat capacity at constant volume, defined in subclasses
+        Internal heat capacity at constant volume, defined in subclasses
         """
 
     def c_p(self, T):
         """
-        Unit heat capacity at constant pressure
+        Heat capacity at constant pressure
+        $$C_{p} = \frac{k}{m} + C_{v}$$
         """
-        return self.R_specific + self.c_v(T)
+        return self.constants["k_B"] / self.mass + self.c_v(T)
 
     def c_p_tr(self, T=None):
         """
-        Translational unit heat capacity at constant pressure
+        Translational heat capacity at constant pressure
+        $$C_{p,tr} = \frac{5}{2}\frac{k}{m}$$
         """
-        return (5 / 2) * self.R_specific
+        return (5 / 2) * self.constants["k_B"] / self.mass
 
     def c_p_int(self, T):
         """
-        Translational unit heat capacity at constant pressure
+        Translational heat capacity at constant pressure
+        $$C_{p,int} = C_{p}-C_{p,tr}$$
         """
         return self.c_p(T) - self.c_p_tr(T)
 
     def e(self, T):
         """
         Full unit energy
+        $$e = e_{tr} + e_{int}$$
         """
         return self.e_tr(T) + self.e_int(T)
 
     def e_tr(self, T):
         """
         Translational unit energy
+        $$e_{tr} = \frac{3}{2}\frac{k}{m}T$$
         """
-        return (3 / 2) * self.R_specific * T
+        return (3 / 2) * (self.constants["k_B"] / self.mass) * T
 
     @abstractmethod
     def e_int(self, T):
@@ -220,24 +235,78 @@ class Particle:
         with open(parameters_path, "r", encoding="UTF-8") as file:
             cls.parameters = json.load(file)
 
+    def _pressure_to_temperature(self, p):
+        """
+        Convert pressure to temperature using the ideal gas law.
+        $$p = nkT$$
+
+        Parameters:
+        - p: Pressure in Pascals (Pa)
+
+        Returns:
+        Temperature in Kelvin (K)
+        """
+        # Ensure pressure and the Boltzmann constant are positive
+        if p <= 0:
+            raise ValueError("Pressure and Boltzmann constant must be positive.")
+
+        # Calculate temperature using the ideal gas law
+        T = p / (self.mole_number * self.constants["k_B"])
+
+        return T
+
+    def _check_pressure_and_temperature(self, T, p):
+        """
+        Check which of temperature and pressure if provided,
+        and convert pressure to temperature if needed.
+        Parameters:
+        - p: Pressure in Pascals
+        - T: Temperature in Kelvin
+
+        Returns:
+        Temperature in Kelvin
+        """
+        if T is not None and p is not None:
+            raise ValueError(
+                "Provide either temperature or pressure, not both. (p=value)"
+            )
+
+        # If pressure is provided, convert it to temperature
+        if p is not None:
+            if p <= 0:
+                raise ValueError("Pressure must be positive.")
+            T = self._pressure_to_temperature(p)
+
+        if T <= 0:
+            raise ValueError("Temperature must be positive.")
+
+        return T
+
 
 class Molecule(Particle):
     """
     Class for a molecule
     """
 
-    n_max = None
-    i_max = None
-    j_max = None
+    _n_max_list = []
+    _i_max_list = []
+    _j_max_list = []
 
     def __init__(self, name):
         super().__init__(name)
         self._find_max_possible_nij()
 
+    def _get_allowed_levels(self, n, i=None):
+        if i is None:
+            return self._j_max_list[n]
+        else:
+            return self._j_max_list[n][i]
+
     def _sum_over_gnij(self, expr):
         """
         Sum an expression multiplied by g_{i,j,n}:
         $$\sum_{nij}g^{c}_{n}g^{c}_{i}g^{c}_{j} expr(n,i,j)$$
+        $$g_{i} = 1, g_{j} = 2j+1, g_{n} = 2n+1$$
 
         Parameters:
         - expr: function, depending on n,i,j
@@ -253,17 +322,75 @@ class Molecule(Particle):
             return 2 * j + 1
 
         def g_n(n):  # g_{n} electronic
+            if self.parameters["g_n_tabulated"]:
+                return self.particle_data["g_n"][n]
             return 2 * n + 1
 
         res = sum(
-            g_i(i + 1) * g_j(j + 1) * g_n(n + 1) * expr(n, i, j)
-            for n in range(self.n_max)
-            for i in range(self.i_max)
-            for j in range(self.j_max)
+            g_i(i) * g_j(j) * g_n(n) * expr(n, i, j)
+            for n in self._n_max_list
+            for i in self._i_max_list[n]
+            for j in self._j_max_list[n][i]
         )
         return res
 
-    def Z_int(self, T):
+    def _find_max_possible_nij(self):
+        """
+        Finding maximum possible energy state based on dissociation energy E_diss,
+        for each electronic level n finds maximum i vibrational level,
+        then for each vibrational level i on electronic level n finds maximum rotational level j
+        """
+        hc = self.constants["h"] * self.constants["c"]
+        n_max_list = []
+        i_max_list = []
+        j_max_list = []
+
+        for n in range(
+            self.parameters["limit_energy_levels"]["molecule"]["electronic"]
+        ):
+            E_diss = self.particle_data["E_diss"][n] * hc * CM_TO_M  # converting to J
+
+            if self._eps(n, 0, 0) >= E_diss:
+                break
+
+            n_max = n
+            n_max_list.append(n_max)
+
+            i_max_list_for_n = []
+            j_max_list_for_n = []
+            for i in range(
+                self.parameters["limit_energy_levels"]["molecule"]["vibrational"]
+            ):
+                if self._eps(n, i, 0) >= E_diss or (
+                    i != 0 and self._eps_v(n, i) < self._eps_v(n, i - 1)
+                ):
+                    break
+
+                i_max = i
+                i_max_list_for_n.append(i_max)
+
+                j_max_list_for_i = []
+                for j in range(
+                    self.parameters["limit_energy_levels"]["molecule"]["rotational"]
+                ):
+                    if self._eps(n, i, j) >= E_diss or (
+                        j != 0 and self._eps_r(n, i, j) < self._eps_r(n, i, j - 1)
+                    ):
+                        break
+
+                    j_max = j
+                    j_max_list_for_i.append(j_max)
+
+                j_max_list_for_n.append(j_max_list_for_i)
+
+            j_max_list.append(j_max_list_for_n)
+            i_max_list.append(i_max_list_for_n)
+
+        self._i_max_list = i_max_list
+        self._j_max_list = j_max_list
+        self._n_max_list = n_max_list
+
+    def Z_int(self, T, p=None):
         """
         The internal partition function
         $$Z_{int,c} = \frac{1}{\sigma}
@@ -271,52 +398,26 @@ class Molecule(Particle):
 
         Parameters:
         - T: Temperature in Kelvin
+        - p: Pressure in Pascal
 
         Returns:
         Partition function for internal energy
         """
-        if self.n_max is None or self.i_max is None or self.j_max is None:
-            self._find_max_possible_nij()
+
+        T = self._check_pressure_and_temperature(T, p)
+
+        if self.parameters["enable_symmetry_factor"]:
+            sigma = self.particle_data["sigma"]
+        else:
+            sigma = 1
 
         def expression(n, i, j):
             return np.exp(-self._eps(n, i, j) / (self.constants["k_B"] * T))
 
-        Z = self._sum_over_gnij(expression) / self.particle_data["sigma"]
-
+        Z = self._sum_over_gnij(expression) / sigma
         return Z
 
-    def _find_max_possible_nij(self):
-        """
-        Finding maximum possible energy state based on dissociation energy E_diss
-        """
-        n_max = 0
-        i_max = 0
-        j_max = 0
-        hc = self.constants["h"] * self.constants["c"]
-        for n in range(self.parameters["energy_levels"]["molecule"]["electronic"]):
-            E_diss = self.particle_data["E_diss"][n] * hc * CM_TO_M
-            if self._eps(n, 0, 0) >= E_diss:
-                break
-            n_max = n
-            for i in range(self.parameters["energy_levels"]["molecule"]["vibrational"]):
-                if self._eps(n, i, 0) >= E_diss or (
-                    i != 0 and self._eps_v(n, i) < self._eps_v(n, i - 1)
-                ):
-                    break
-                i_max = i
-                for j in range(
-                    self.parameters["energy_levels"]["molecule"]["rotational"]
-                ):
-                    if self._eps(n, i, j) >= E_diss or (
-                        j != 0 and self._eps_r(n, i, j) < self._eps_r(n, i, j - 1)
-                    ):
-                        break
-                j_max = j
-        self.n_max = n_max
-        self.i_max = i_max
-        self.j_max = j_max
-
-    def e_int(self, T):
+    def e_int(self, T, p=None):
         """
         Unit internal energy
         $$e_{int,c} =
@@ -324,54 +425,58 @@ class Molecule(Particle):
 
         Parameters:
         - T: Temperature in Kelvin
+        - p: Pressure in Pascal
 
         Returns:
         Unit internal energy
         """
-        emZ = self._sum_over_gnij(
-            lambda n, i, j: self._eps(n, i, j)
-            * np.exp(-self._eps(n, i, j) / (self.constants["k_B"] * T))
-        )
-        e = emZ / ((self.mass / self.constants["N_a"]) * self.Z_int(T))
+        T = self._check_pressure_and_temperature(T, p)
+
+        def summand(n, i, j):
+            return self._eps(n, i, j) * np.exp(
+                -self._eps(n, i, j) / (self.constants["k_B"] * T)
+            )
+
+        emZ = self._sum_over_gnij(summand)
+        e = emZ / ((self.mass) * self.Z_int(T))
         return e
 
-    def c_v_int(self, T):
+    def c_v_int(self, T, p=None):
         """
         Internal unit heat capacity at constant volume
+        $$C_{v,int} = \frac{dE_{v,int}}{dT}$$
 
          Parameters:
         - T: Temperature in Kelvin
+        - p: Pressure in Pascals
 
         Returns:
         Internal unit heat capacity at constant volume
 
         """
         k = self.constants["k_B"]
-        term1 = self._sum_over_gnij(
-            lambda n, i, j: self._eps(n, i, j) ** 2
-            * np.exp(-self._eps(n, i, j) / (k * T))
-            / (k * T) ** 2
-        ) / self.Z_int(T)
-        term2 = self._sum_over_gnij(
-            lambda n, i, j: self._eps(n, i, j)
-            * np.exp(-self._eps(n, i, j) / (k * T))
-            / (k * T)
-        ) / self.Z_int(T)
-        return (
-            self.constants["k_B"]
-            / (self.mass / self.constants["N_a"])
-            * (term1 - term2**2)
-        )
+        T = self._check_pressure_and_temperature(T, p)
+
+        def summand1(n, i, j):
+            return self._eps(n, i, j) ** 2 * np.exp(-self._eps(n, i, j) / (k * T))
+
+        def summand2(n, i, j):
+            return self._eps(n, i, j) * np.exp(-self._eps(n, i, j) / (k * T))
+
+        term1 = self._sum_over_gnij(summand1) / (k * T) ** 2 / self.Z_int(T)
+        term2 = self._sum_over_gnij(summand2) / (k * T) / self.Z_int(T)
+
+        return (term1 - term2**2) * self.constants["k_B"] / (self.mass)
 
 
 class Atom(Particle):
     """
-    Class for a atom
+    Class for an atom
     """
 
     def __init__(self, name):
         super().__init__(name)
-        self.n_max = self.parameters["energy_levels"]["atom"]["electronic"]
+        self.n_max = self.parameters["limit_energy_levels"]["atom"]["electronic"]
 
     def _sum_over_gn(self, term):
         """
@@ -404,7 +509,7 @@ class Atom(Particle):
             lambda n: self._eps_el(n)
             * np.exp(-self._eps_el(n) / (self.constants["k_B"] * T))
         )
-        e = emZ / ((self.mass / self.constants["N_a"]) * self.Z_int(T))
+        e = emZ / (self.mass * self.Z_int(T))
         return e
 
     def c_v_int(self, T):
@@ -428,11 +533,7 @@ class Atom(Particle):
         term2 = self._sum_over_gn(
             lambda n: self._eps_el(n) * np.exp(-self._eps_el(n) / (k * T)) / (k * T)
         ) / self.Z_int(T)
-        return (
-            self.constants["k_B"]
-            / (self.mass / self.constants["N_a"])
-            * (term1 - term2**2)
-        )
+        return self.constants["k_B"] / self.mass * (term1 - term2**2)
 
     def Z_int(self, T):
         """
--- main.py
+++ main.py
@@ -6,34 +6,32 @@ Particle.load_particle_data()
 T1 = 298.15
 T2 = 1000
 N2 = Molecule("N2")
-N = Atom("N")
 T = T1
 
-
-print(
-    f"Results for N2 at Temperature: {T} K with "
-    f"{N2.n_max+1} electronic states, {N2.i_max+1} vibrational energy levels, "
-    f"and {N2.j_max+1} rotational energy levels:"
-)
-
+print(f"Results for N2 at Temperature: {T}")
+# print(N2._get_allowed_levels(0))
 print("\nPartition Functions:")
 print("Z_tr:", N2.Z_tr(T))
-print("Z_int:", N2.Z_int(T))
+print("Z_int:", N2.Z_int(T))  # = 5288 Capitelli at 298.15
 print("Total Partition Function (Z):", N2.Z(T))
 
-print("\nSpecific Heat Capacities (J/(mol·K)):")
-print("c_v_tr:", N2.c_v_tr(T))
-print("c_v_int:", N2.c_v_int(T))
-print("Total c_v:", N2.c_v(T))
-
-print("\nc_p_int:", N2.c_p_int(T))
-print("Total c_p:", N2.c_p(T))
-
-print("\nInternal Energy (J/mol):")
+print("\nInternal Energy:")
 print("e_tr:", N2.e_tr(T))
 print("e_int:", N2.e_int(T))
 print("Total Internal Energy (e):", N2.e(T))
 
+print("\nHeat Capacities:")
+print("c_v_tr:", N2.c_v_tr(T))
+print("c_v_int:", N2.c_v_int(T))
+print("Total c_v:", N2.c_v(T))
+
+print("\nc_p_tr:", N2.c_p_tr(T))
+print("c_p_int:", N2.c_p_int(T))
+print("Total c_p:", N2.c_p(T))
+# C_p = 2911 J/mol/K  = 103890 J/kg/K Capitelli at 298.15
+
 print("\nAdditional Relations:")
-print("c_p_int/R:", N2.c_p_int(T) / N2.R_specific)
-print("e_int/RT:", N2.e_int(T) / (T * N2.R_specific))
+print("c_p_int/R:", N2.c_p_int(T) / N2.constants["R"])
+# e_int/RT = 0.9976 Capitelli at 298.15
+print("e_int/RT:", N2.e_int(T) / (T * N2.constants["R"]))
+# C_p_int/R = 1.003 Capitelli at 298.15
